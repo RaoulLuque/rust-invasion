ab165386ae9f4a15cbe669441d6b30791ea484dc
Raoul Luqué <raoulsluque@gmail.com>
2024-02-12 21:44:54 +0100

    Made reaching screen border teleport to other end of screen

--- src/player.rs
+++ src/player.rs
@@ -5,11 +5,15 @@ use systems::*;
 
 use bevy::prelude::*;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemSet)]
+struct AfterMovement;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
-            .add_systems(Update, player_movement);
+            .add_systems(Update, constrain_player_movement.in_set(AfterMovement))
+            .add_systems(Update, player_movement.before(AfterMovement));
     }
 }
--- src/player/systems.rs
+++ src/player/systems.rs
@@ -1,5 +1,5 @@
 use crate::player::components::Player;
-use bevy::{input::keyboard, prelude::*, window::PrimaryWindow};
+use bevy::{input::keyboard, prelude::*, transform, window::PrimaryWindow};
 
 pub const PLAYER_HEIGHT: f32 = 75.0;
 pub const PLAYER_WIDTH: f32 = 112.0;
@@ -39,5 +39,28 @@ pub fn player_movement(
         }
 
         transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn constrain_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query
+        .get_single()
+        .expect("Primary window should exist");
+
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let x_min = -window.width() / 2.0;
+        let x_max = -x_min;
+
+        let player_translation_x_value: &mut f32 = &mut player_transform.translation.x;
+
+        // Let player appear on right side of screen
+        if *player_translation_x_value < x_min {
+            *player_translation_x_value += window.width();
+        } else if *player_translation_x_value > x_max {
+            *player_translation_x_value -= window.width();
+        }
     }
 }
